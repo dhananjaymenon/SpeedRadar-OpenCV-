72989c8942ca148f2be41080b984d3b552761d57
Dhananjay Sunil Menon <72432304+dhananjaymenon@users.noreply.github.com>
2022-11-24 23:32:50 -0600

    Update tracker2.py

--- tracker2.py
+++ tracker2.py
@@ -2,10 +2,19 @@ import cv2
 import math
 import time
 import numpy as np
+import os
 
-limit = 80 #km/hr
+limit = 80  # km/hr
 
-file = open("D://TrafficRecord//SpeedRecord.txt","w")
+traffic_record_folder_name = "TrafficRecord"
+
+if not os.path.exists(traffic_record_folder_name):
+    os.makedirs(traffic_record_folder_name)
+    os.makedirs(traffic_record_folder_name+"//exceeded")
+
+
+speed_record_file_location = traffic_record_folder_name + "//SpeedRecord.txt"
+file = open(speed_record_file_location, "w")
 file.write("ID \t SPEED\n------\t-------\n")
 file.close()
 
@@ -16,17 +25,16 @@ class EuclideanDistTracker:
         self.center_points = {}
 
         self.id_count = 0
-        #self.start = 0
-        #self.stop = 0
-        self.et=0
-        self.s1 = np.zeros((1,1000))
-        self.s2 = np.zeros((1,1000))
-        self.s = np.zeros((1,1000))
+        # self.start = 0
+        # self.stop = 0
+        self.et = 0
+        self.s1 = np.zeros((1, 1000))
+        self.s2 = np.zeros((1, 1000))
+        self.s = np.zeros((1, 1000))
         self.f = np.zeros(1000)
         self.capf = np.zeros(1000)
         self.count = 0
         self.exceeded = 0
-
 
     def update(self, objects_rect):
         objects_bbs_ids = []
@@ -37,7 +45,7 @@ class EuclideanDistTracker:
             cx = (x + x + w) // 2
             cy = (y + y + h) // 2
 
-            #CHECK IF OBJECT IS DETECTED ALREADY
+            # CHECK IF OBJECT IS DETECTED ALREADY
             same_object_detected = False
 
             for id, pt in self.center_points.items():
@@ -48,28 +56,27 @@ class EuclideanDistTracker:
                     objects_bbs_ids.append([x, y, w, h, id])
                     same_object_detected = True
 
-                    #START TIMER
+                    # START TIMER
                     if (y >= 410 and y <= 430):
-                        self.s1[0,id] = time.time()
+                        self.s1[0, id] = time.time()
 
-                    #STOP TIMER and FIND DIFFERENCE
+                    # STOP TIMER and FIND DIFFERENCE
                     if (y >= 235 and y <= 255):
-                        self.s2[0,id] = time.time()
-                        self.s[0,id] = self.s2[0,id] - self.s1[0,id]
+                        self.s2[0, id] = time.time()
+                        self.s[0, id] = self.s2[0, id] - self.s1[0, id]
 
-                    #CAPTURE FLAG
-                    if (y<235):
-                        self.f[id]=1
+                    # CAPTURE FLAG
+                    if (y < 235):
+                        self.f[id] = 1
 
-
-            #NEW OBJECT DETECTION
+            # NEW OBJECT DETECTION
             if same_object_detected is False:
                 self.center_points[self.id_count] = (cx, cy)
                 objects_bbs_ids.append([x, y, w, h, self.id_count])
                 self.id_count += 1
-                self.s[0,self.id_count]=0
-                self.s1[0,self.id_count]=0
-                self.s2[0,self.id_count]=0
+                self.s[0, self.id_count] = 0
+                self.s1[0, self.id_count] = 0
+                self.s2[0, self.id_count] = 0
 
         # ASSIGN NEW ID to OBJECT
         new_center_points = {}
@@ -81,49 +88,46 @@ class EuclideanDistTracker:
         self.center_points = new_center_points.copy()
         return objects_bbs_ids
 
-    #SPEEED FUNCTION
-    def getsp(self,id):
-        if (self.s[0,id]!=0):
+    # SPEEED FUNCTION
+    def getsp(self, id):
+        if (self.s[0, id] != 0):
             s = 214.15 / self.s[0, id]
         else:
             s = 0
 
         return int(s)
 
-    #SAVE VEHICLE DATA
-    def capture(self,img,x,y,h,w,sp,id):
-        if(self.capf[id]==0):
+    # SAVE VEHICLE DATA
+    def capture(self, img, x, y, h, w, sp, id):
+        if (self.capf[id] == 0):
             self.capf[id] = 1
-            self.f[id]=0
-            crop_img = img[y-5:y + h+5, x-5:x + w+5]
-            n = str(id)+"_speed_"+str(sp)
-            file = 'D://TrafficRecord//' + n + '.jpg'
+            self.f[id] = 0
+            crop_img = img[y - 5:y + h + 5, x - 5:x + w + 5]
+            n = str(id) + "_speed_" + str(sp)
+            file = traffic_record_folder_name + '//' + n + '.jpg'
             cv2.imwrite(file, crop_img)
             self.count += 1
-            filet = open("D://TrafficRecord//SpeedRecord.txt", "a")
-            if(sp>limit):
-                file2 = 'D://TrafficRecord//exceeded//' + n + '.jpg'
+            filet = open(speed_record_file_location, "a")
+            if (sp > limit):
+                file2 = traffic_record_folder_name + '//exceeded//' + n + '.jpg'
                 cv2.imwrite(file2, crop_img)
-                filet.write(str(id)+" \t "+str(sp)+"<---exceeded\n")
-                self.exceeded+=1
+                filet.write(str(id) + " \t " + str(sp) + "<---exceeded\n")
+                self.exceeded += 1
             else:
                 filet.write(str(id) + " \t " + str(sp) + "\n")
             filet.close()
 
-
-    #SPEED_LIMIT
+    # SPEED_LIMIT
     def limit(self):
         return limit
 
-    #TEXT FILE SUMMARY
+    # TEXT FILE SUMMARY
     def end(self):
-        file = open("D://TrafficRecord//SpeedRecord.txt", "a")
+        file = open(speed_record_file_location, "a")
         file.write("\n-------------\n")
         file.write("-------------\n")
         file.write("SUMMARY\n")
         file.write("-------------\n")
-        file.write("Total Vehicles :\t"+str(self.count)+"\n")
-        file.write("Exceeded speed limit :\t"+str(self.exceeded))
+        file.write("Total Vehicles :\t" + str(self.count) + "\n")
+        file.write("Exceeded speed limit :\t" + str(self.exceeded))
         file.close()
-
-
