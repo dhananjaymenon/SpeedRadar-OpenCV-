f975fbe2e6f397de31d60ac61863440429815f6e
Dhananjay Sunil Menon <72432304+dhananjaymenon@users.noreply.github.com>
2023-08-20 16:07:37 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from SpeedRadar import run_speed_tracker
+
+
+def main():
+    video_file_location = "Resources/traffic4.mp4"
+    run_speed_tracker(video_file_location)
+
+
+if __name__ == '__main__':
+    main()
